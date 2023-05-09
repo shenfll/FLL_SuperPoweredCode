0d98c8173dcf67a47fbccad33eab40977816c179
shenfll <81711385+shenfll@users.noreply.github.com>
2023-05-09 16:21:08 -0400

    Update SuperPowerMissionCode.py
    Our new code is amazing. You will love it 100%. :D

--- SuperPowerMissionCode.py
+++ SuperPowerMissionCode.py
@@ -1,18 +1,15 @@
-﻿# Copyright 2023 Shenendehowa CSD FLL team #3249 the CaptiBytes
+# Copyright 2023 Shenendehowa CSD FLL team #3249 the CaptiBytes
 # Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 # 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
 # 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-
 # The following code by Shenendehowa CSD FLL team #3249 the CaptiBytes is a python program designed to be run on a Lego Spike Prime robot. It completes the SuperPowered robot game missions from the 2022-2023 FLL season. This code can be found on GitHub in a repository owned by user ShenFLL.
-
 
 from math import *
 import hub
 import uos
 import utime
-
 
 # ports object
 ports = {
@@ -30,32 +27,58 @@ ports = {
     "f": hub.port.F
 }
 
-
 # speed multiplier
-speed = 1.025
+speed = 1
 
+# all-new speeeeeed stuff
+curves = []
 
 # define running
 running = False
 
-
 # set drive motors
 drive = ports["D"].motor.pair(ports["E"].motor)
-
 
 # set attachment motors
 attachment = ports["A"].motor.pair(ports["C"].motor)
 
+# linear speed curve functions
+# linearspeedcurve(slope,y-intercept)
+def linearspeedcurve(m,b):
+    curves.append("linear m = "+str(m)+" b = "+str(b))
+    return len(curves)-1
+
+# calculate speed curve function (plz don't call)
+def calculatespeedcurve(curve,x):
+    if "linear" in curve:
+        m_start = curve.index(" m = ")
+        b_start = curve.index(" b = ")
+        m = float(curve[m_start+5:b_start])
+        b = float(curve[b_start+5:])
+        return m*x+b
 
 # move tank function
 # mt(distance in degrees,left motor speed,right motor speed)
-def mt(amount,ls,rs,wait = True):
+def mt(amount,ls,rs,curve = -1,wait = True):
+    start = drive.primary().get()[1]
+    current = drive.primary().get()[1]
+    percent = abs(current-start)/amount
+    currentspeed = speed
+    if(curve > -1):
+        currentspeed *= calculatespeedcurve(curves[curve],percent)
+    drive.run_at_speed(round(-ls*currentspeed),round(rs*currentspeed))
+    while abs(current-start) < amount:
+        current = drive.primary().get()[1]
+        percent = abs(current-start)/amount
+        currentspeed = speed
+        if(curve > -1):
+            currentspeed *= calculatespeedcurve(curves[curve],percent)
+        drive.run_at_speed(round(-ls*currentspeed),round(rs*currentspeed))
+    drive.hold()
+
+def oldmt(amount,ls,rs,wait = True):
     drive.run_for_degrees(round(amount),speed_0=round(-ls*speed),speed_1=round(rs*speed))
     global running
-
-
-
-
     running = True
     def event(details):
         if details == drive.primary().EVENT_COMPLETED:
@@ -64,28 +87,24 @@ def mt(amount,ls,rs,wait = True):
     drive.callback(event)
     while running and wait:
         pass
-
+    
 
 # start tank function
 # st(left motor speed,right motor speed)
 def st(ls,rs):
     drive.run_at_speed(round(-ls*speed),round(rs*speed))
 
-
 # stop tank function
 def pt():
     drive.hold()
-
 
 # reset drive function
 def rt():
     drive.preset(0,0)
 
-
 # reset attachments function
 def ra():
     attachment.preset(0,0)
-
 
 # move attachment function
 # ma(port letter of motor,distance in degrees,motor speed)
@@ -94,17 +113,14 @@ def ma(ml,amount,ms,wait = True):
     if wait:
         mwt(ml)
 
-
 # start attachment function
 # sa(port letter of motor,motor speed)
 def sa(ml,ms):
     ports[ml].motor.run_at_speed(round(ms))
 
-
 # pause attachment function
 def pa(ml):
     ports[ml].motor.hold()
-
 
 # move attachments sync function
 # mas(distance in degrees,left motor speed,right motor speed)
@@ -120,39 +136,32 @@ def mas(amount,ls,rs,wait = True):
     while running and wait:
         pass
 
-
 # get distance
 def gd():
     return round((abs(drive.primary().get()[1]) + abs(drive.secondary().get()[1])) / 2)
-
 
 # get reflected light function
 # rl(port letter of light sensor)
 def rl(ll):
     return ports[ll].device.get()[0]
 
-
 # wait function
 # wt(time in milliseconds)
 def wt(ms):
     utime.sleep_ms(ms)
-
 
 # motor wait function (internal plz don't touch)
 def mwt(ml):
     while ports[ml].motor.busy(ports[ml].motor.BUSY_MOTOR):
         pass
 
-
 # get gyro angle function
 def ga():
     return hub.motion.yaw_pitch_roll()[0]
 
-
 # reset gyro function (plz don't call with arguments, all arguments are internal)
 def rg(ang = 0, yc = 0):
     hub.motion.yaw_pitch_roll(ang,yc)
-
 
 # calibrate gyro function
 def cg():
@@ -162,7 +171,6 @@ def cg():
     mt(500,-25,-25)
     rg(yc = ga() / 5)
 
-
 # line follow function
 # lf(port letter of light sensor,distance in degrees,speed,acceleration/gain,side of line)
 def lf(ln,amount,ts,ac,side):
@@ -170,8 +178,6 @@ def lf(ln,amount,ts,ac,side):
     x = 0
     t = utime.time()
     rt()
-
-
     while gd() < amount:
         if abs(70-rl(ln)) > 10:
             ac += 0.00001 * ts
@@ -189,21 +195,24 @@ def lf(ln,amount,ts,ac,side):
     pt()
     print(x/(utime.time()-t))
 
-
 # gyro follow function
 # gf(distance in degrees,speed,acceleration/gain,heading in degrees,direction forward (1) or backward (0))
-def gf(amount,ts,ac,ang,dn):
-    ts *= speed
+def gf(amount,tso,ac,ang,dn,curve = -1):
+    ts = tso * speed
+    if curve>-1:
+        ts *= calculatespeedcurve(curves[curve],0)
     rt()
     while gd() < amount:
         left = (ang-ga())*10
         right = (ga()-ang)*10
+        ts = tso*speed
+        if curve > -1:
+            ts *= calculatespeedcurve(curves[curve],gd()/amount)
         if dn == 1:
             st(left*ac+ts,right*ac+ts)
         else:
             st(right*-ac-ts,left*-ac-ts)
     pt()
-
 
 # gyro turn function
 # gt(angle to turn to)
@@ -217,28 +226,59 @@ def gt(ang):
                 st((ang-ga()-70)/(i+10),(ga()-ang+70)/(i+10))
     pt()
 
-
 # play sound function
 # sound(index of sound file in the sounds folder)
 def sound(sn):
     hub.sound.play("/sounds/"+uos.listdir("/sounds")[sn])
-
 
 # set hub led color function
 # led(integer that corresponds to some color)
 def led(n):
     hub.led(n)
 
-
 # exit function
 def exit():
     raise SystemExit
-
 
 # get temperature function
 def temp():
     return hub.temperature()*9/5+32
 
+# "the best line follower i ever made" function
+# tblfiem(length,light sensor,fastness,multiplier,target,diagnostics)
+# example: tblfiem(1000,"B",25,0.15,71)
+def tblfiem(l,s,f,m,t,d = False):
+    average = []
+    start = abs(drive.primary().get()[1])
+    current = start
+    while current-start < l:
+        light = rl(s)
+        error = light - t
+        if d:
+            average.append(error)
+            print(sum(average)/len(average))
+        st(f+error*m,f-error*m)
+        current = abs(drive.primary().get()[1])
+    pt()
+
+# "the best line follower i ever made" function
+# tblfiem(length,light sensor,fastness,multiplier,target,diagnostics)
+# example: tblfiem(1000,"B",25,0.15,71)
+def lefty(l,s,f,m,t,d = False):
+    average = []
+    start = abs(drive.primary().get()[1])
+    current = start
+    while current-start < l:
+        light = rl(s)
+        error = light - t
+        #white error: 30
+        #black error: -30
+        if d:
+            average.append(error)
+            print(sum(average)/len(average))
+        st(f+error*m,f-error*m)
+        current = abs(drive.primary().get()[1])
+    pt()
 
 # purple mission function
 def purple(imaquitter):
@@ -246,14 +286,14 @@ def purple(imaquitter):
     if imaquitter:
         return
     rg()
-    gf(980,45,0.1,0,1)
+    gf(980,60,0.1,0,1,curve=linearspeedcurve(-(1/2),1))
     gf(200,60,0.1,0,0)
     gt(-40)
     gf(960,40,0.05,-40,1)
     gt(45)
     gf(800,30,0.05,45,1)
     gf(200,50,0.05,45,0)
-    for i in range(3):
+    for i in range(2):
         gf(450,40,0.05,45,1)
         wt(300)
         gf(200,40,0.05,45,0)
@@ -262,14 +302,13 @@ def purple(imaquitter):
     gt(-55)
     ma("C",400,-100)
     wt(300)
-    gf(500,25,0.1,-55,1)
-    gt(-30)
+    gf(380,25,0.1,-55,1)
+    gt(-38)
     ma("C",350,50)
     ma("C",300,-50)
     gt(-45)
+    ma("C",500,100,wait = False)
     mt(2000,-70,-70)
-    ma("C",500,100)
-
 
 # orange mission function
 def orange(imaquitter):
@@ -284,7 +323,6 @@ def orange(imaquitter):
     wt(200)
     gf(1400,-70,0.1,0,1)
 
-
 # blue mission function
 def blue(imaquitter):
     led(3)
@@ -298,15 +336,14 @@ def blue(imaquitter):
     gt(0)
     gf(850,50,0.1,0,1)
     gf(550,-50,0.1,0,1)
-    gt(-40)
-    gf(800,30,0.1,-41,1)
+    gt(-35)
+    gf(800,30,0.1,-35,1)
     gt(-15)
     gf(200,30,0.1,-15,1)
     gt(-90)
-    gf(350,30,0.1,-90,1)
+    gf(300,30,0.1,-90,1)
     gt(-145)
     gf(1700,65,0.1,-145,1)
-
 
 # pink mission function
 def pink(imaquitter):
@@ -314,13 +351,13 @@ def pink(imaquitter):
     if imaquitter:
         return
     rg()
-    gf(700,25,0.1,0,1)
-    ma("A",150,100)
-    wt(200)
-    ma("C",350,100)
-    ma("A",80,-100)
-    gf(900,-70,0.1,0,1)
-
+    gf(250,50,0.1,0,1)
+    gt(45)
+    gf(400,20,0.1,45,1)
+    ma("C",200,80)
+    gf(150,50,0.1,45,1)
+    ma("A",100,-100)
+    gf(600,-80,0.1,45,1)
 
 # yellow mission function
 def yellow(imaquitter):
@@ -328,7 +365,17 @@ def yellow(imaquitter):
     if imaquitter:
         return
     rg()
-    gf(800,35,0.08,0,1)
+    gf(750,40,0.08,0,1)
+    gt(25)
+    lefty(725,"F",20,0.25,90)
+    gf(100,40,0.08,0,1)
+    for i in range(3):
+        ma("A",500,-100)
+        ma("A",500,100)
+    gf(500,100,0.08,0,0)
+    gt(30)
+    gf(1100,100,0.08,30,0)
+    """gf(800,35,0.08,0,1)
     gt(45)
     gf(400,35,0.08,45,1)
     gt(0)
@@ -340,7 +387,7 @@ def yellow(imaquitter):
     gt(45)
     gf(400,75,0.08,45,0)
     gt(0)
-    gf(800,75,0.08,0,0)
+    gf(800,75,0.08,0,0)"""
 
 
 # green mission function
@@ -349,21 +396,19 @@ def green(imaquitter):
     if imaquitter:
         return
     rg()
-    gf(100,30,0.1,0,1)
+    gf(150,30,0.1,0,1)
     gt(65)
     wt(200)
     gf(1340,40,0.1,65,1)
     gt(-40)
     wt(200)
     gf(200,35,0.1,-40,1)
-    gf(50,-20,0.1,-40,1)
-    ma("A",200,100)
+    gf(30,-20,0.1,-40,1)
+    ma("A",250,100)
     gt(0)
-    ma("C",90,10)
     gf(300,-50,0.1,0,1)
     gt(90)
     gf(2000,70,0.1,90,1)
-
 
 # red mission function
 def red(imaquitter):
@@ -372,60 +417,58 @@ def red(imaquitter):
         return
     rg()
     mt(1000,40,40)
+    ma("A",110,100)
     mt(200,-40,-40)
 
-
 numbers = [hub.Image("""00900
-09900
-90900
 00900
-99999
-"""),hub.Image("""99990
-00009
-00990
-09000
-99999
-"""),hub.Image("""99990
-00009
-99990
-00009
-99990
-"""),hub.Image("""09090
-90090
-99999
-00090
-00090
-"""),hub.Image("""99999
-90000
-99990
-00009
-99990
-"""),hub.Image("""09999
-90000
-99990
-90009
-09990
-"""),hub.Image("""99999
-00090
 00900
-09000
-90000
+00900
+00900
 """),hub.Image("""09990
-90009
+00090
 09990
-90009
+09000
 09990
-"""),hub.Image("""099990
-90009
-09999
-00009
-00009
+"""),hub.Image("""09990
+00090
+09990
+00090
+09990
+"""),hub.Image("""09090
+09090
+09990
+00090
+00090
+"""),hub.Image("""09990
+09000
+09990
+00090
+09990
+"""),hub.Image("""09990
+09000
+09990
+09090
+09990
+"""),hub.Image("""09990
+00090
+00090
+00090
+00090
+"""),hub.Image("""09990
+09090
+09990
+09090
+09990
+"""),hub.Image("""09990
+09090
+09990
+00090
+00090
 """)]
-
 
 # list containing order of missions
 programs = [purple,orange,blue,pink,yellow,green,red]
-
 
 # main function manages inputs, display, and running programs 
 def main():
@@ -460,8 +503,6 @@ def main():
             programs[program](False)
             program += 1
     exit()
-
-
 
 
 # run everything
